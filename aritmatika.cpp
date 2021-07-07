3211d6346881bf013714074a6a34bee4f206c2fb
riyopj <87062785+riyopj@users.noreply.github.com>
2021-07-07 14:53:53 +0700

    Update aritmatika.cpp

--- aritmatika.cpp
+++ aritmatika.cpp
@@ -166,3 +166,147 @@ int main(){
 	
 	return 0;
 }
+
+
+
+#include <iostream> // no 3
+#include <stack>
+#include <ctype.h>
+using namespace std;
+long unsigned int i;
+
+bool isOp(char x){
+	if(x=='+'||x=='-'||x=='*'||x=='/'||x=='%')
+		return true;
+	return false;
+}
+int precedence(char x){
+	if(x=='+'||x=='-') return 1;
+	if(x=='*'||x=='/'||x=='%') return 2;
+	return 0;
+}
+double applyOp(double num1, double num2, char op){
+    switch(op){
+        case '+': return num1 + num2;
+        case '-': return num1 - num2;
+        case '*': return num1 * num2;
+        case '/': return num1 / num2;
+        }
+    return 0;
+}
+void calculate(string input){
+	stack<double> data;
+	stack<char>operasi;
+	
+	for(i=0; i<input.length(); i++){
+		if(isdigit(input[i])){
+			int value=0;
+			while(i<input.length() && isdigit(input[i])){
+				value = (value*10) + (input[i] - '0');
+				i++;
+			}
+			i--;
+			data.push(value);
+		}
+		else if(input[i] == '(')
+			operasi.push(input[i]);
+		else if(input[i] == ')'){
+			while(!operasi.empty() && operasi.top() != '('){
+				if(operasi.top() == '%'){
+					int value2 = data.top(); data.pop();
+                    int value1 = data.top(); data.pop();
+                    operasi.pop();
+                    data.push(value1%value2);
+				} else {
+					double value2 = data.top(); data.pop();
+                    double value1 = data.top(); data.pop();
+                    char op = operasi.top(); operasi.pop();
+                    data.push(applyOp(value1, value2, op));
+				}
+			}
+			if(!operasi.empty())
+				operasi.pop();
+		} else {
+			if(input[i] == '-'){
+				if(i==0){
+					if(isdigit(input[i+1])){
+						i++;
+						int value=0;
+						while(i<input.length() && isdigit(input[i])){
+							value = (value*10) + (input[i] - '0');
+							i++;
+						}
+						i--;
+						data.push(value*-1);
+					} else {
+						data.push(-1);
+						operasi.push('*');
+					}
+				} else {
+					while(!operasi.empty() && precedence(operasi.top()) >= precedence(input[i]) && !isOp(input[i-1])){
+						if(operasi.top() == '%'){
+							int value2 = data.top(); data.pop();
+		                    int value1 = data.top(); data.pop();
+		                    operasi.pop();
+		                    data.push(value1%value2);
+						} else {
+							double value2 = data.top(); data.pop();
+		                    double value1 = data.top(); data.pop();
+		                    char op = operasi.top(); operasi.pop();
+		                    data.push(applyOp(value1, value2, op));
+						}
+					}
+					if((isdigit(input[i+1]) || input[i+1]=='(') && (isdigit(input[i-1]) || input[i-1]==')'))
+						operasi.push(input[i]);
+					else{
+						data.push(-1);
+						operasi.push('*');
+					}
+				} 
+			} else {
+				while(!operasi.empty() && precedence(operasi.top()) >= precedence(input[i])){
+					if(operasi.top() == '%'){
+						int value2 = data.top(); data.pop();
+		                int value1 = data.top(); data.pop();
+		                operasi.pop();
+		                data.push(value1%value2);
+					} else {
+						double value2 = data.top(); data.pop();
+		                double value1 = data.top(); data.pop();
+		                char op = operasi.top(); operasi.pop();
+		                data.push(applyOp(value1, value2, op));
+					}
+				}
+				operasi.push(input[i]);
+			}
+		}
+	}
+	while(!operasi.empty()){
+		if(operasi.top() == '%'){
+			int value2 = data.top(); data.pop();
+		    int value1 = data.top(); data.pop();
+		    operasi.pop();
+		    data.push(value1%value2);
+		} else {
+			double value2 = data.top(); data.pop();
+		    double value1 = data.top(); data.pop();
+		    char op = operasi.top(); operasi.pop();
+		    data.push(applyOp(value1, value2, op));
+		}
+	}
+	cout << data.top() << endl;
+}
+
+int main(){
+	string ekspresi; getline(cin,ekspresi);
+	string new_ekspresi="";
+	for(i=0;i<ekspresi.length();i++){
+		if(ekspresi[i]==' ')
+			continue;
+		else
+			new_ekspresi+=ekspresi[i];
+	}
+	calculate(new_ekspresi);
+	
+	return 0;
+}
